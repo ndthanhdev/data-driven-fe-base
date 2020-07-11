4011096566477f21fc6a8d445df29e78eb51d3e8
ndthanhdev <ndthanhdev@outlook.com>
2020-07-12 01:35:05 +0700

    up

--- pkgs/app/src/pages/NotFound/NotFound.tsx
+++ pkgs/app/src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,27 @@
 import * as React from 'react'
 import * as Mui from '@material-ui/core'
+import * as MuiIcons from '@material-ui/icons'
+
+const useClasess = Mui.makeStyles({
+	zero: {
+		fontSize: 'inherit',
+	},
+})
 
 export const NotFoundPage: React.FC = () => {
-	return <Mui.Typography variant="h1"> 404 </Mui.Typography>
+	const classes = useClasess()
+	return (
+		<Mui.Box
+			display="flex"
+			flexDirection="column"
+			justifyContent="center"
+			alignItems="center"
+			p="3rem"
+		>
+			<Mui.Typography variant="h1">
+				4<MuiIcons.GitHub className={classes.zero} />4
+			</Mui.Typography>
+			<Mui.Typography variant="h6">Didn't find anything here</Mui.Typography>
+		</Mui.Box>
+	)
 }
