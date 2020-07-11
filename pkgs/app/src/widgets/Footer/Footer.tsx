7e2060bed27e76e2958bdee4932d4031a011830d
ndthanhdev <ndthanhdev@outlook.com>
2020-07-12 01:25:15 +0700

    up

--- pkgs/app/src/widgets/Footer/Footer.tsx
+++ pkgs/app/src/widgets/Footer/Footer.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react'
 import * as Mui from '@material-ui/core'
+import * as MuiIcons from '@material-ui/icons'
 
 export const Footer: React.FC = () => {
-	return <>footer</>
+	return (
+		<>
+			<Mui.Box
+				display="flex"
+				width="100%"
+				height="6rem"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<MuiIcons.GitHub />
+			</Mui.Box>
+		</>
+	)
 }
